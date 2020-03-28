d848f9a42f75e85e144713efe2e48a60b5faa9ab
Tiago Patricio <tiagoudp@gmail.com>
2020-03-28 13:57:19 +0000

    Updates

---
+++ precache-manifest.50c13853d01724c57b2f8417c64e1ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081689eaf9839b83ec7276eb871552b8",
+    "url": "/tiagopatricio/galo-app.git/index.html"
+  },
+  {
+    "revision": "1c1f9de9081811200aeb",
+    "url": "/tiagopatricio/galo-app.git/static/css/main.08c6cc07.chunk.css"
+  },
+  {
+    "revision": "e9c1ff5c5b159be849f4",
+    "url": "/tiagopatricio/galo-app.git/static/js/2.cba874e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiagopatricio/galo-app.git/static/js/2.cba874e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1f9de9081811200aeb",
+    "url": "/tiagopatricio/galo-app.git/static/js/main.f3ebceca.chunk.js"
+  },
+  {
+    "revision": "407dc9e835085e05fb97",
+    "url": "/tiagopatricio/galo-app.git/static/js/runtime-main.2285a959.js"
+  }
+]);
